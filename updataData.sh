64d3f6798f25e94c0a4a7cc825b7068d37d84f80
Andreas Lauser <and@poware.org>
2020-05-10 21:47:48 +0200

    add tiny shell script to clone and update the input data

---
+++ updataData.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+pushd . > /dev/null
+if test -d "COVID-19"; then
+    cd COVID-19
+    git pull
+else
+    git clone https://github.com/CSSEGISandData/COVID-19
+fi
+
+popd > /dev/null
+    
