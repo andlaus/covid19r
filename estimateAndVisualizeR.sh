2a1edb3966cd94431ebc1d3e93488317970630a9
Andreas Lauser <and@poware.org>
2020-05-15 15:50:29 +0200

    estimateAndVisualizeR.sh: allow to show arbitrary fields

--- estimateAndVisualizeR.sh
+++ estimateAndVisualizeR.sh
@@ -1,13 +1,23 @@
 #! /bin/bash
 
-if test "$#" != 1; then
-    echo "Usage: $0 COUNTRY_NAME"
+if test "$#" -lt 1; then
+    echo "Usage: $0 COUNTRY_NAME [FIELDS_TO_PLOT]"
     exit 1
 fi
 
 COUNTRY="$1"
 
-./estimateR.py "$COUNTRY" > "r-estimate-$COUNTRY.csv"
+FIELDS="6 7"
+RANGE="set yrange [:2.5];"
+if test "$#" -gt 1; then
+    FIELDS="${@:2}"
+    RANGE=""
+    echo "Using custom fields: $FIELDS"
+fi
+
+OUTPUT_DIR="processed-data"
+
+./estimateR.py "$COUNTRY" > "$OUTPUT_DIR/$COUNTRY.csv"
 
 cat > /tmp/"visualize-R-$COUNTRY.gnuplot" <<EOF
 
@@ -17,14 +27,21 @@ set timefmt "%Y-%m-%d"
 #set xrange ["2020-04-25":"2020-12-31"]
 set format x "%Y/%m/%d"
 
-set yrange [0:2.5]
+$RANGE
 
 set title "Curves for $COUNTRY"
-
-plot "r-estimate-$COUNTRY.csv" using 1:6 w lp, "r-estimate-$COUNTRY.csv" using 1:7 w lp
-
-pause -1 "press ENTER to finish"
-
 EOF
+
+echo -n "plot " >> /tmp/"visualize-R-$COUNTRY.gnuplot"
+
+for FIELD in $FIELDS; do
+    echo -n "\"$OUTPUT_DIR/$COUNTRY.csv\" using 1:$FIELD w lp, " >> /tmp/"visualize-R-$COUNTRY.gnuplot"
+done
+
+echo >> /tmp/"visualize-R-$COUNTRY.gnuplot"
+echo >> /tmp/"visualize-R-$COUNTRY.gnuplot"
+
+echo "pause -1 \"press ENTER to finish\"" >> /tmp/"visualize-R-$COUNTRY.gnuplot"
+
 
 gnuplot /tmp/"visualize-R-$COUNTRY.gnuplot"
