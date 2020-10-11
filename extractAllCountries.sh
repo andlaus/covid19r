db869a95080d3bb3e864b701bebd44123908276e
Andreas Lauser <and@poware.org>
2020-10-11 14:10:28 +0200

    improve the all coutries extraction script
    now, if something goes wrong with a country, the old data files will no longer linger around.

--- extractAllCountries.sh
+++ extractAllCountries.sh
@@ -7,16 +7,17 @@ OUTPUT_DIR="processed-data"
 
 mkdir -p "$OUTPUT_DIR"
 
+rm -rf "$OUTPUT_DIR/*"
+
 echo > "$OUTPUT_DIR/countries.csv"
 
 ./estimateRAll.py
 
-for BLA in $(ls *.csv | grep -v country-populations); do
-    mv "$BLA" "$OUTPUT_DIR"
-done
-
 for COUNTRY in $(./listCountries.py | tail -n +2); do
     echo "$COUNTRY,$(./countryPopulation.py $COUNTRY)" >> "$OUTPUT_DIR/countries.csv" 
+
+    mv "$COUNTRY.csv" "$OUTPUT_DIR/"
+
     echo -n "$COUNTRY: "
     tail -n +1 "$OUTPUT_DIR/$COUNTRY.csv"| wc -l
 done
