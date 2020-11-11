cb0fabfb0058540388319ed2e3a2073d8b842440
Andreas Lauser <and@poware.org>
2020-11-11 17:27:12 +0100

    fix offset calculation for adaptive smoother
    the last value of the curve was not considered. there seem to be about
    a billion of things that can go wrong and all of them do...

--- dashboard-functions.js
+++ dashboard-functions.js
@@ -486,7 +486,7 @@ function smoothenDataAdaptive(d) {
 
         // compute transition factor
         if (i > d.length - n - 1) {
-            offset = n/2 + n/2*(1.0 - (d.length - i - 1)/n);
+            offset = (n-1)/2 + (n-1)/2*(1.0 - (d.length - i - 1)/n);
             beta = offset - Math.floor(offset);
             offset = Math.floor(offset);
         }
