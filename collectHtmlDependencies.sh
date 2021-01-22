bcce8f1b11dc9dc764b4432b459bc1d834267668
Andreas Lauser <and@poware.org>
2021-01-22 14:15:21 +0100

    update the html dependency collection script
    NPM is great!

--- collectHtmlDependencies.sh
+++ collectHtmlDependencies.sh
@@ -7,6 +7,8 @@ cp --parents node_modules/select2/dist/css/select2.min.css html/
 cp --parents node_modules/select2/dist/js/select2.min.js html/
 cp --parents node_modules/bootstrap/dist/js/bootstrap.min.js html/
 cp --parents node_modules/bootstrap/dist/css/bootstrap.css html/
+cp --parents node_modules/bootstrap/dist/css/bootstrap.css.map html/
+cp --parents node_modules/bootstrap/dist/js/bootstrap.js html/
 cp --parents node_modules/@fortawesome/fontawesome-free/css/all.css html/
 cp --parents -R node_modules/@fortawesome/fontawesome-free/webfonts/ html/
 cp --parents *.{js,svg,html,css} html/
